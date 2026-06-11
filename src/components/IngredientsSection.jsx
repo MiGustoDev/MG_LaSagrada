bbda11a113b8b30564c3685446fbc2077d9201e1
Facu14carrizo <Facu14carrizo@gmail.com>
2026-06-11 16:26:39 -0300

    add right justify in text

--- src/components/IngredientsSection.jsx
+++ src/components/IngredientsSection.jsx
@@ -230,7 +230,7 @@ export default function IngredientsSection() {
               />
             </div>
 
-            <div className="space-y-3 text-white/90 font-body-md text-xs md:text-sm md:max-lg:text-base leading-relaxed text-justify -mt-3 md:-mt-5">
+            <div className="space-y-3 text-white/90 font-body-md text-xs md:text-sm md:max-lg:text-base leading-relaxed text-center -mt-3 md:-mt-5">
               <p>
                 Hay países donde el fútbol se mira. Y después estamos los argentinos, donde el fútbol se vive con pasión. Acá el Mundial es cábala, grito antes del gol, nervios compartidos y discusión por quién baja a abrirle al delivery en medio del partido.
               </p>
